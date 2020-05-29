4ef33c8eacbfd354f693b9a69f46d3c21185e91b
Benjoe Lanorio <benjoelanorio@gmail.com>
2020-05-29 13:14:44 +0800

    added HOC Spinner

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument
+  // addCollectionAndDocuments 
+} from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { createStructuredSelector } from 'reselect';
-
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
 
@@ -24,6 +29,7 @@ class App extends React.Component {
 
   componentDidMount() {
 
+    // const { setCurrentUser, collectionsArray } = this.props;
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -41,7 +47,7 @@ class App extends React.Component {
         });
       } else {
         setCurrentUser(userAuth);
-
+        // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })))
       }
     });
   }
@@ -78,6 +84,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+  // collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,20 +4,23 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.style.scss';
 
-const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
-      {
-        items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))
-      }
+const CollectionPreview = ({ title, items, ...otherProps }) => {
+  console.log(otherProps)
+  return (
+    <div className='collection-preview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {
+          items
+            .filter((item, idx) => idx < 4)
+            .map((item) => (
+              <CollectionItem key={item.id} item={item} />
+            ))
+        }
 
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default CollectionPreview;
