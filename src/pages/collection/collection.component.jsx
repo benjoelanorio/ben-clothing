529bd9cdcd99e72512b8c87e8bb170f2da0428dd
benjoelanorio <benjoelanorio@gmail.com>
2020-06-11 18:27:13 +0800

    updated App, sign-in and sign-up to use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,9 +5,24 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
+// import { firestore } from '../../firebase/firebase.utils';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+  // EXAMPLE OF COMPONENT WILL UNMOUNT EQUIVALENT
+  // useEffect(() => {
+  //   console.log('[useEffect] : I am subscribing');
+  //   const unsubscribeFromCollections = firestore
+  //     .collection('collections')
+  //     .onSnapshot(snapshot => console.log('[SNAPSHOT]', snapshot));
+
+  //   return () => {
+  //     console.log('[useEffect] : I am Unsubscribing')
+  //     unsubscribeFromCollections();
+  //   }
+  // }, [])
+
   const { title, items } = collection;
   return (
     <div className='collection-page'>
